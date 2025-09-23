35c4a0abb79a81a82e5264f4eb47afb901de6c8d
Michael Vanderpoorten <michael@yungpanda.com>
2025-09-23 17:04:02 +0200

    Week 1 les 1 deel 1

---
+++ week-1/les-1/start.js
@@ -0,0 +1,14 @@
+console.log("Hallo iedereen");
+
+console.log("Welkom bij Programming 1");
+
+// aanmaken variabele student met inhoud tekst Alazar
+const student = "Lucas"; // string
+const age = 20; // number
+console.log("Welkom " + student);
+console.log(student + " is " + age + " jaar oud");
+
+student = "Yben";
+
+const nextYear = age + 1;
+console.log("Volgend jaar is " + student + " " + nextYear + " jaar oud");
